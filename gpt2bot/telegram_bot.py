0ba14f585660d739a099c2806cfa1eb063899fb4
carloshalviarez1995 <129683400+carloshalviarez1995@users.noreply.github.com>
2023-04-02 13:01:58 -0500

    Update telegram_bot.py

--- gpt2bot/telegram_bot.py
+++ gpt2bot/telegram_bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters, PicklePersistence
-from telegram import ChatAction
+from telegram.constants import ChatAction
 from functools import wraps
 from urllib.parse import urlencode
 import requests
